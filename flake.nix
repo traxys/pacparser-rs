7bb08a5b79b7bf5ec91b1b39e84677940b801e5d
traxys <quentin+dev@familleboyer.net>
2022-09-09 19:43:52 +0200

    Remove LIBCLANG from flake.nix

--- flake.nix
+++ flake.nix
@@ -26,17 +26,6 @@
           rust
         ];
         buildInputs = [];
-
-        shellHook = with pkgs; ''
-          export LIBCLANG_PATH="${pkgs.llvmPackages_11.libclang.lib}/lib";
-          export BINDGEN_EXTRA_CLANG_ARGS="$(< ${stdenv.cc}/nix-support/libc-crt1-cflags) \
-              $(< ${stdenv.cc}/nix-support/libc-cflags) \
-              $(< ${stdenv.cc}/nix-support/cc-cflags) \
-              $(< ${stdenv.cc}/nix-support/libcxx-cxxflags) \
-              ${lib.optionalString stdenv.cc.isClang "-idirafter ${stdenv.cc.cc}/lib/clang/${lib.getVersion stdenv.cc.cc}/include"} \
-              ${lib.optionalString stdenv.cc.isGNU "-isystem ${stdenv.cc.cc}/include/c++/${lib.getVersion stdenv.cc.cc} -isystem ${stdenv.cc.cc}/include/c++/${lib.getVersion stdenv.cc.cc}/${stdenv.hostPlatform.config} -idirafter ${stdenv.cc.cc}/lib/gcc/${stdenv.hostPlatform.config}/${lib.getVersion stdenv.cc.cc}/include"} \
-            "
-        '';
       };
     });
 }
